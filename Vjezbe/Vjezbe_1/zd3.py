84b6e276d6753c4951499ac833da7d7275c2a753
KloDragun <kdragun@pmfst.hr>
2023-03-08 18:44:27 +0100

    Zadatak 3

---
+++ Vjezbe/Vjezbe_1/zd3.py
@@ -0,0 +1,32 @@
+
+def unos():
+    while(1):
+        tocka=[input("Upisite koordinatu x za tocku"),input("Upisite koordinatu y za tocku")]
+        c1=0
+        test=[tocka[0].replace('.','',1).replace('-','',1),tocka[1].replace('.','',1).replace('-','',1)]
+        if test[0].isdigit()==True and test[1].isdigit()==True:
+            print("Da")
+            c1=1
+            break
+        else:
+            print("Upisite ponovo koordinate tocke")
+    return tocka
+
+
+print("Upisite koordinate za tocku 1")
+tocka1=unos()
+print("Upisite koordinate za tocku 2")
+tocka2=unos()
+
+x1=float(tocka1[0])
+y1=float(tocka1[1])
+x2=float(tocka2[0])
+y2=float(tocka2[1])
+
+k=float((y2-y1)/(x2-x1))
+print(k)
+
+print("Jednadzba pravca kroz tocke {} i {} je:\n".format(tocka1,tocka2))
+print("y={}x+({})".format(k,-k*x1+y1))
+
+
