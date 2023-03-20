33588397ab4ab25255500ec9a9e03d2a7c41a0f4
KloDragun <kdragun@pmfst.hr>
2023-03-20 09:59:38 +0100

    Preostao zad 3-b(arithm) i lingress

--- Vjezbe/Vjezbe_3/arithm.py
+++ Vjezbe/Vjezbe_3/arithm.py
@@ -3,14 +3,14 @@ import numpy as np
 #Par tocaka, znamo da je aritmeticka 1.5
 tocke=np.linspace(1.4,1.6,10)
 
-#Racunjanje sredine
+#Računanje sredine
 sredina=0
 for i in range(len(tocke)):
     sredina=sredina+tocke[i]
 
 sredina=sredina/len(tocke)
 
-
+#Računanje derivacije
 devijacija=0
 for i in range(len(tocke)):
     devijacija=devijacija+(tocke[i]-sredina)**2
