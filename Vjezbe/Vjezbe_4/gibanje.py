d1e052de916a39db8b6b7fa7785d7f49629b04af
KloDragun <kdragun@pmfst.hr>
2023-03-31 12:51:00 +0200

    Vj4 test

---
+++ Vjezbe/Vjezbe_4/gibanje.py
@@ -0,0 +1,9 @@
+import particle as par
+
+t1=par.Particle(10,60,0,0,0.02)
+
+t1.plot_trajectory()
+
+
+#For petlja sa dometom za razl d-t-ove, izracunat apsolutnu gresku
+
--- Vjezbe/Vjezbe_4/particle.py
+++ Vjezbe/Vjezbe_4/particle.py
@@ -1,33 +1,56 @@
 import numpy as np
+import matplotlib.pyplot as plt
 g=9.81
 class Particle:
-    def __init__(c,v,fi,x,y):
+
+    def __init__(c,v,fi,x,y,dt):
         c.brzina=v
-        c.kut=fi
-        c.koordx=x
-        c.koordy=y
-
-    def print(c):
-        print(c.brzina)
-        print(c.kut)
-        print(c.koordx,c.koordy)
-
+        c.kut=np.deg2rad(fi)
+        c.koordx=[x]
+        c.koordy=[y]
+        c.brzinay=[v*np.sin(c.kut)]
+        c.tuk=0
+        c.domet=0
+        c.dt=dt
     def reset(c):
-        c.brzina=0
-        c.kut=0
-        c.koord=0
+        c.koordx=[c.koordx[0]]
+        c.koordy=[c.koordy[0]]
+        c.brzinay=[c.brzinay[0]]
+        c.tuk=0
+        c.domet=0
+    
+    #Ovaj reset se koristi kod range f-je
+    def tempreset(c):
+        c.koordx=[c.koordx[0]]
+        c.koordy=[c.koordy[0]]
+        c.brzinay=[c.brzinay[0]]
+        c.tuk=0
 
     def __move(c):
-        dt=0.02
-        c.koordx=c.koordx+c.brzina*dt*np.cos(c.kut)
-        c.koordy=c.koordy+c.brzina*dt*np.sin(c.kut)-0.5*g*(dt**2) #analiticki, ne ovako
+        c.tuk=c.tuk+c.dt
+        c.brzinay.append(c.brzinay[-1]-g*c.dt)
+        c.koordx.append(c.koordx[-1]+c.brzina*c.dt*np.cos(c.kut))
+        c.koordy.append(c.koordy[-1]+c.brzinay[-1]*c.dt)
 
     def evolve(c):
-        while(c.koordy>0):
+        while(c.koordy[-1]>=0):
             c.__move()
 
+        del c.brzinay[-1]
+        del c.koordx[-1]
+        del c.koordy[-1]
 
-p1=Particle(10,60,2,3)
-p1.print()
-p1.evolve()
-p1.print()
+    def range(c):
+        c.evolve()
+        c.domet=c.koordx[-1]
+        print(c.domet)
+        c.tempreset()
+
+    def plot_trajectory(c):
+        c.evolve()
+        plt.plot(c.koordx,c.koordy)
+        plt.title("x-y graf")
+        plt.xlabel("x-os [m]")
+        plt.ylabel("y-os [m]")
+        plt.show()
+        c.reset()
