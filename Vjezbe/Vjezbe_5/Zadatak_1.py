49d2a8cba47aa9453dc32718e5c039fd131994f2
KloDragun <kdragun@pmfst.hr>
2023-04-26 09:43:41 +0200

    Zad 1 final i nacrt zad2

--- Vjezbe/Vjezbe_5/Zadatak_1.py
+++ Vjezbe/Vjezbe_5/Zadatak_1.py
@@ -2,22 +2,29 @@ import numpy as np
 import calculus as cal
 import matplotlib.pyplot as plt
 
-
-
+#Upisemo funkciju
+def funkcija(x):
+    return np.sin(x)
 
 #Za testiranje
-x,y=cal.granica(0,10,0.01)
-t,z=cal.granica(0,10,0.1)
-o,i=cal.granica(0,10,0.3)
+x,y=cal.granica(funkcija,0,10,0.03)
+t,z=cal.granica(funkcija,0,10,0.8)
+k,l=cal.granica(funkcija,0,10,2)
 test=[]
 for i in x:
     test.append(np.cos(i))
 
 #Plottanje
-plt.plot(x,y)
-plt.plot(t,z)
-plt.plot(o,i)
-plt.plot(x,test)
+fig, ax = plt.subplots(figsize=(12,4))
+
+ax.plot(x,test, linestyle='--', linewidth=3,label='Analiticki')
+ax.plot(x,y,markersize=5,color='green' ,label='Korak: 0.03')
+ax.plot(t,z,markersize=2,color='blue' ,label='Korak: 0.8')
+ax.plot(k,l,markersize=4,color='orange' ,label='Korak: 2')
+
+ax.set_title('Derivacija funkcije ovisno o koracima')
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)')
+
+ax.legend()
 plt.show()
-
-
--- Vjezbe/Vjezbe_5/calculus.py
+++ Vjezbe/Vjezbe_5/calculus.py
@@ -2,11 +2,11 @@ import numpy as np
 #Pocetni parametar
 h=0.01
 
-#Zeljena funkcija, testno
+#f-ja za debug
 def funkcija(x):
-    return np.sin(x)
+    return x**2
 
-#1. Metoda, malo weird jer trazi da primimo citavu f-ju a 2. metoda ne trazi
+#1. Metoda
 def derivacija(funk,tocka,ts=0):
     if ts=='y':
         return (funk(tocka+h)-funk(tocka))/h
@@ -14,14 +14,33 @@ def derivacija(funk,tocka,ts=0):
         return (funk(tocka+h)-funk(tocka-h))/(2*h)
 
 #2. Metoda
-def granica(donja,gornja,korak,ts=0):
+def granica(funk,donja,gornja,korak,ts=0):
     tocke=np.arange(donja,gornja+korak,korak)
     derivacije=[]
     if ts=='y':
         for i in tocke:
-            derivacije.append((funkcija(i+korak)-funkcija(i))/korak)
+            derivacije.append((funk(i+korak)-funk(i))/korak)
     else:
         for i in tocke:
-            derivacije.append((funkcija(i+korak)-funkcija(i-korak))/(2*korak))
+            derivacije.append((funk(i+korak)-funk(i-korak))/(2*korak))
     return tocke,derivacije
 
+#3. Metoda 
+def pravokutna(funk,donja_granica,gornja_granica,podjele):
+    #Lista tocaka za integraciju+korak između točaka
+    tocke=np.linspace(donja_granica,gornja_granica,podjele)
+    dx=tocke[1]-tocke[0]
+
+    gornja_medja=0
+    for i in tocke:
+        gornja_medja+=funk(i)*dx
+    gornja_medja=gornja_medja-funk(tocke[0])
+
+    donja_medja=0
+    for i in tocke:
+        donja_medja+=funk(i)*dx
+    donja_medja=donja_medja-funk(tocke[-1])
+    print(gornja_medja)
+    print(donja_medja)
+
+pravokutna(funkcija,1,4,100)
