26a1e1f633405dfbaa6589838461e89cb4fd9f09
KloDragun <kdragun@pmfst.hr>
2023-05-25 11:18:20 +0200

    Vjezbe 7 final

--- Vjezbe/Vjezbe_7/Projectile.py
+++ Vjezbe/Vjezbe_7/Projectile.py
@@ -10,8 +10,6 @@ class Projectile:
         c.Cd=Cd
         c.m=m
         c.A=A
-        c.x=x
-        c.y=y
         c.kut=np.deg2rad(fi)
         c.koordx=[x]
         c.koordy=[y]
@@ -20,17 +18,7 @@ class Projectile:
         c.akcelx=[-1*np.sign(c.brzinax[0])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinax[0])**2]
         c.akcely=[-9.81-np.sign(c.brzinay[0])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[0])**2]
 
-        c.t=[0]
         c.dt=dt
-
-    #Reset, za bug testove
-    def reset(c):
-        c.koordx=[c.koordx[0]]
-        c.koordy=[c.koordy[0]]
-        c.brzinay=[c.brzina*np.sin(c.kut)]
-        c.brzinax=[c.brzina*np.cos(c.kut)]
-        c.akcelx=[-1*np.sign(c.brzinax[0])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinax[0])**2]
-        c.akcely=[-9.81-np.sign(c.brzinay[0])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[0])**2]
 
     #Euler
     def Euler_move(c):
@@ -41,7 +29,6 @@ class Projectile:
             c.brzinay.append(c.brzinay[-1]+c.akcely[-1]*c.dt)
             c.akcelx.append(-1*np.sign(c.brzinax[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinax[-1])**2)
             c.akcely.append(-9.81-np.sign(c.brzinay[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[-1])**2)
-            print(c.koordy[-1],c.t[-1])
 
 
     #R-k
@@ -49,19 +36,20 @@ class Projectile:
         while c.koordy[-1]>=0:
             kvx=[(-1*np.sign(c.brzinax[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinax[-1])**2)*c.dt]
             kvy=[(-9.81-np.sign(c.brzinay[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[-1])**2)*c.dt]
-            kx=[c.koordx[-1]*c.dt]
-            ky=[c.koordy[-1]*c.dt]
-
+            kx=[c.brzinax[-1]*c.dt]
+            ky=[c.brzinay[-1]*c.dt]
+        
             for i in range(3):
                 kvx.append((-1*np.sign(c.brzinax[-1]+0.5*kvx[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinax[-1]+0.5*kvx[-1])**2)*c.dt)
-                kvy.append((-9.81-np.sign(c.brzinay[-1]+0.5*kvy[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[-1]+0.5*kvy[-1])**2)*c.dt)
+                kvy.append((-9.81-np.sign(c.brzinay[-1]+0.5*kvy[-1])*(c.ro*c.Cd*c.A/(2*c.m))*(c.brzinay[-1]+kvy[-1])**2)*c.dt)
                 kx.append((c.brzinax[-1]+0.5*kvx[-1])*c.dt)
                 ky.append((c.brzinay[-1]+0.5*kvy[-1])*c.dt)
-
+                
             c.brzinax.append(c.brzinax[-1]+(kvx[0]+2*kvx[1]+2*kvx[2]+kvx[3])/6)
             c.brzinay.append(c.brzinay[-1]+(kvy[0]+2*kvy[1]+2*kvy[2]+kvy[3])/6)
             c.koordx.append(c.koordx[-1]+(kx[0]+2*kx[1]+2*kx[2]+kx[3])/6)
             c.koordy.append(c.koordy[-1]+(ky[0]+2*ky[1]+2*ky[2]+ky[3])/6)
+
 
 
     #Plottanje
@@ -71,24 +59,34 @@ class Projectile:
             plt.plot(c.koordx,c.koordy)
             plt.title("x-y graf")
             plt.xlabel("x [m]")
-            plt.ylabel("y[m]")
-            plt.show()
-            print("YES")
+            plt.ylabel("y [m]")
         else:
             c.Euler_move()
             plt.plot(c.koordx,c.koordy)
             plt.title("x-y graf")
             plt.xlabel("x [m]")
-            plt.ylabel("y[m]")
-            plt.show()
-            print("yes")
+            plt.ylabel("y [m]")
 
 
 
     
+#Inputi za plottanje
+t1=Projectile(10,20,0,0,0.001,2.3,0.6,0.05,4)
+t2=Projectile(10,20,0,0,0.1,2.3,0.6,0.05,4)
+t3=Projectile(10,20,0,0,0.2,2.3,0.6,0.05,4)
+t4=Projectile(10,20,0,0,0.01,2.3,0.6,0.05,4)
+t5=Projectile(10,20,0,0,0.01,2.3,0.6,0.05,4)
 
-t1=Projectile(10,20,0,0,0.01,2.3,0.6,0.05,4)
+t1.Plot(2)
+t2.Plot(2)
+t3.Plot(2)
+t4.Plot(2)
+t5.Plot()
 
-t1.Plot()
+
+plt.legend(['dt=0.001 s', 'dt=0.1s', 'dt=0.2s','dt=0.01s', 'dt=0.01s, Rk'])
+plt.show()
 
 
+#Kao što se iz grafa vidi, nefizičko gibanje imamo za dt=0.2(ili priblizno tome)
+#Iz slike se vidi da je Runge-Kutteova metoda preciznija(Rk je blize 0.001)
